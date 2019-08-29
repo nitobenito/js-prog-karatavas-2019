38a8924af151f2c14b853390165404ab7c6efc9a
Pavils Jurjans <passiday@gmail.com>
2019-08-29 15:54:32 +0300

    Struktura mazliet papildinata

--- js/main.js
+++ js/main.js
@@ -36,21 +36,39 @@ class Karatavas {
   }
   jaunaSpele() {
     console.log("Sākam jaunu spēli!");
+    this.statuss = Karatavas.STATUSS_JAUNS;
+    this.jaunsProgress(0);
+    this.jaunsUzdevums();
+    this.atjaunotRebusu();
+    console.log("Vēl ir jāsaliek burtu pogas!");
   }
   jaunsProgress(limenis) {
-    console.log("Jauns progress:", limenis);
+    this._progress = limenis;
+    console.log("Jauns progress:", this._progress);
+    if (this.konteiners) {
+      this.divProgress.innerHTML = "Progress: " + this._progress;
+    }
   }
   jaunsUzdevums() {
-    console.log("Jauns uzdevums!");
+    this.uzdevums = "UZDEVUMS";
+    console.log("Jauns uzdevums: ", this.uzdevums);
+    return this.uzdevums;
   }
   atjaunotRebusu() {
     console.log("Atjaunojam rebusu");
   }
   minBurtu(burts) {
+    burts = burts.toUpperCase().charAt(0);
     console.log("Minam burtu:", burts);
   }
   beigas(uzvara) {
-    console.log("Spēles beigas!", uzvara ? " Uzvarējām" : "Zaudējām");
+    if (uzvara) {
+      this.statuss = Karatavas.STATUSS_UZVARA;
+      console.log("Uzvara!");
+    } else {
+      this.statuss = Karatavas.STATUSS_SAGRAVE;
+      console.log("Sagrāve!");
+    }
   }
 }
 
