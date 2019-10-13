0a0ddcd26e9fd95276edbd8f54b4322e17833b9b
Pavils Jurjans <passiday@gmail.com>
2019-10-13 20:43:36 +0300

    Ieviests uzdevuma vārdu saraksts.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import json
+from random import choice
+
 app = Flask(__name__, static_url_path='')
 
 # Uzstada index.html ka nokluseto failu.
@@ -9,7 +11,19 @@ def root():
 
 @app.route('/api/uzdevums')
 def api_vards():
+    uzdevumi = [
+        'ĀBOLS',
+        'APELSĪNS',
+        'BANĀNS',
+        'CITRONS',
+        'ČIEKURS',
+        'DATELE',
+        'EĻĻA',
+        'ĒRKŠĶOGA',
+        'FRIKADELE',
+        'GURĶIS'
+    ]
     response = {
-        'uzdevums': 'UZDEVUMS'
+        'uzdevums': choice(uzdevumi)
     }
     return json.dumps(response)
